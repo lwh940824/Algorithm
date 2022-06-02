6bda2e77c4100efd91345ec1623834ab76638dfe
Lee Won Hee <lwh940824@gmail.com>
2022-06-03 01:46:58 +0900

    Q_13335-트럭 풀이중

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException{
-		Q_2750 q_2750 = new Q_2750();
-    q_2750.solution();
+		Q_13335 q_13335 = new Q_13335();
+    q_13335.solution();
 	}
 }
